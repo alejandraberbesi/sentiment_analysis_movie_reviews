1d4cef4ccca8167678fb7fcb502a55f2cd2c3836
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-10-20 19:33:48 -0500

    some code changes

--- src/sentiment_mod.py
+++ src/sentiment_mod.py
@@ -8,21 +8,23 @@ from statistics import mode
 import string
 from nltk.corpus import stopwords
 import os
+#nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
 
-
-# Context Manager for movie reviews
-with open("txt/positive.txt","rb") as obj:
+# Context Manager to open movie reviews .txt files
+with open("../txt/positive.txt","rb") as obj:
     short_pos = str(obj.read())
-with open("txt/negative.txt","rb") as obj:
+with open("../txt/negative.txt","rb") as obj:
     short_neg = str(obj.read())
 
+#cleaning step
 short_pos=short_pos.replace('\\r','')
 short_neg=short_neg.replace('\\r','')
 
 allowed_word_types=["J"] #adjectives
 reviews_words=[]
-stopwords=stopwords.words('english')
-stopw = set(stopwords)
+stopw = set(stopwords.words('english'))
 exclude=stopw.union(string.punctuation)
 documents=[]
 
@@ -32,20 +34,20 @@ def segment_words(document,typesent):
         words=word_tokenize(p)
         pos_t=nltk.pos_tag(words)
         for w in pos_t:
-            if w[1][0] in allowed_word_types: #first letter has to J,R,V
+            if w[1][0] in allowed_word_types: #first letter has to be J
                 if w[0] not in exclude and w[0].isalpha():
                     reviews_words.append(w[0].lower())  
          
-            
 segment_words(short_pos,"positive")
 segment_words(short_neg,"negative")
-random.shuffle(documents)
 
 #to train against a certain number of words:
 reviews_words = nltk.FreqDist(reviews_words)
-word_features=list(reviews_words.keys())[:500]
+#filter words with at least 5 appearances in text
+word_features=list(filter(lambda x: x[1]>=5,reviews_words.items()))
+
 def find_features(document):
-    words=set(document)
+    words=word_tokenize(document)
     features={}
     for w in word_features: 
         features[w]=(w in words)  
@@ -53,14 +55,15 @@ def find_features(document):
 
 featuresets= [(find_features(rev), category) for (rev, category) in documents]
 
-training_set=featuresets[:8000]
-testing_set=featuresets[8000:]
+random.seed(100)
+random.shuffle(featuresets)
+
+training_set = featuresets[:8000]
+testing_set =  featuresets[8000:]
 
 classifier=nltk.NaiveBayesClassifier.train(training_set)
-MNBclassifier=SklearnClassifier(MultinomialNB())
-MNBclassifier.train(training_set)
-Bernoulli_classifier=SklearnClassifier(BernoulliNB())
-Bernoulli_classifier.train(training_set)
+MNBclassifier=SklearnClassifier(MultinomialNB()).train(training_set)
+Bernoulli_classifier=SklearnClassifier(BernoulliNB()).train(training_set)
 
 #combining algorithms with a vote:
 class voting_process(ClassifierI):
